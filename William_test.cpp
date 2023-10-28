164486124a364c60580c67a327cbdd24f6a0a485
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2023-10-28 14:58:07 +0800

    William's Test

---
+++ William_test.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define sz(v) (int)v.size()
+using namespace std;
+
+const int maxn = (int)2e5 + 5;
+const int INF = (int)1e18 + 5;
+
+
+
+void solve() {
+  cout << "Muan is so dian.\n";
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+  solve();
+
+  return 0;
+}
