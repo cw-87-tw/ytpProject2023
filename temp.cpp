670291a0ce9dd6690e1afc1a29929f39b738eede
oTaiwan <66476837+cw-87-tw@users.noreply.github.com>
2023-10-28 14:35:34 +0800

    test

---
+++ temp.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio ios_base::sync_with_stdio(0); cin.tie(0);
+#define int long long
+#define vt vector
+#define pb emplace_back
+#define sz(x) (int) x.size()
+#define INF 0x3f3f3f3f3f3f3f3f
+#define PI pair<int, int>
+#define rep(i, from, to) for (int i = from; i <= to; ++i)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("avx,avx2,fma")
+const int mxn = 2e5 + 5, MOD = 1e9 + 7;
+
+
+
+signed main(void){
+    #ifdef AutoIO
+    freopen("C:\\Code\\C\\input.txt","r",stdin);
+    freopen("C:\\Code\\C\\output.txt","w",stdout);
+    #endif
+    fio
+    cout << "hello\n";
+}
