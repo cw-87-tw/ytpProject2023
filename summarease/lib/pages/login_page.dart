18b68a05a88523ef6ab2033d75acd0d4d7cf122a
amyfang0404 <amyfang0404@gmail.com>
2024-01-06 17:18:33 +0800

    some bugs

--- summarease/lib/pages/login_page.dart
+++ summarease/lib/pages/login_page.dart
@@ -24,21 +24,21 @@ class _LoginPageState extends State<LoginPage> {
 
   void signUserIn() async {
     
-    showDialog(
-      context: context,
-      builder: (context) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-    );
+    // showDialog(
+    //   context: context,
+    //   builder: (context) {
+    //     return const Center(
+    //       child: CircularProgressIndicator(),
+    //     );
+    //   }
+    // );
     
     await FirebaseAuth.instance.signInWithEmailAndPassword(
       email: email_controller.text,
       password: password_controller.text,
     );
 
-    Navigator.pop(context);
+    // Navigator.pop(context);
   }
 
   @override
