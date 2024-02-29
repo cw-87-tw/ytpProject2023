b3c23e99077e826815dac01dfc615d194335d461
oTaiwan <66476837+cw-87-tw@users.noreply.github.com>
2024-02-29 23:09:01 +0800

    some updates

--- summarease/lib/process/textSummary.dart
+++ summarease/lib/process/textSummary.dart
@@ -12,25 +12,27 @@ Future<String> summarizeText(String text) async {
       'Content-Type': 'application/json',
     },
     body: jsonEncode({
-      "model": "gpt-3.5-turbo",
+      "model": "gpt-4-turbo-preview",
       "messages": [
-      {
-        "role": "system",
-        "content": "You are a helpful assistant. You need to summarize the text given by the user"
-      },
-      {
-        "role": "user",
-        "content": text
-      }
-    ],
+        {
+          "role": "system",
+          "content":
+              "You are a helpful assistant. You need to summarize the text given by the user. But you can only answer all questions in English."
+        },
+        {"role": "user", "content": text},
+      ],
     }),
   );
   print("GPT replied\n\n\n");
   print("Reply Data ${response.body}");
+  // return response.body;
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
-    final summary = data['choices'][0]['message']['content'].toString();
+    // final test = utf8.decode(data);
+    // print("test $test\n\n\n\n");
+    final summary = data['choices'][0]['message']['content'].toSdtring();
     return summary;
+    // return respons;
   } else {
     print("Failed\n");
     return "Failed";
