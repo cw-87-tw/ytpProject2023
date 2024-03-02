b51e69e0a1784c9139c3aa32ad0c0f2c22a92ceb
oTaiwan <66476837+cw-87-tw@users.noreply.github.com>
2024-03-02 21:57:17 +0800

    rm some files

--- summarease/lib/process/textSummary.dart
+++ summarease/lib/process/textSummary.dart
@@ -2,6 +2,44 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'assets.dart';
 
+Future<String> callAPI(String text) async {
+  final apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+  final response = await http.post(
+    Uri.parse(apiUrl),
+    headers: {
+      'Authorization': 'Bearer $apiKey',
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      "model": "gpt-4-turbo-preview",
+      "messages": [
+        {
+          "role": "system",
+          "content":
+              "You are a helpful assistant. You need to summarize the text given by the user. But you can only answer all questions in English."
+        },
+        {"role": "user", "content": text},
+      ],
+      // "response_format": {"type": "json_object"}
+    }),
+  );
+  print("GPT replied\n\n\n");
+  print("Reply Data ${response.body}");
+  // return response.body;
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    final rawData = data['choices'][0]['message']['content'].toString();
+    final summary = utf8.decode(rawData.runes.toList());
+    print("Summary $summary\n\n\n");
+    return summary;
+    // return respons;
+  } else {
+    print("Failed\n");
+    return "Failed";
+  }
+}
+
 Future<String> summarizeText(String text) async {
   final apiUrl = 'https://api.openai.com/v1/chat/completions';
 
