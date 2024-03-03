de444f6335f628c945105db17ababa0876d50bed
oTaiwan <66476837+cw-87-tw@users.noreply.github.com>
2024-03-03 18:31:13 +0800

    new function callAPI for send button to use

--- summarease/lib/process/textSummary.dart
+++ summarease/lib/process/textSummary.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'assets.dart';
 
-Future<String> callAPI(String text) async {
+Future<String> callAPI(List<Map<String, String>> msgs) async {
   final apiUrl = 'https://api.openai.com/v1/chat/completions';
 
   final response = await http.post(
@@ -13,14 +13,7 @@ Future<String> callAPI(String text) async {
     },
     body: jsonEncode({
       "model": "gpt-4-turbo-preview",
-      "messages": [
-        {
-          "role": "system",
-          "content":
-              "You are a helpful assistant. You need to summarize the text given by the user. But you can only answer all questions in English."
-        },
-        {"role": "user", "content": text},
-      ],
+      "messages": msgs,
       // "response_format": {"type": "json_object"}
     }),
   );
@@ -41,39 +34,13 @@ Future<String> callAPI(String text) async {
 }
 
 Future<String> summarizeText(String text) async {
-  final apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-  final response = await http.post(
-    Uri.parse(apiUrl),
-    headers: {
-      'Authorization': 'Bearer $apiKey',
-      'Content-Type': 'application/json',
+  List<Map<String, String>> msgs = [
+    {
+      "role": "system",
+      "content":
+          "You are a helpful assistant. You need to summarize the text given by the user. But you can only answer all questions in English."
     },
-    body: jsonEncode({
-      "model": "gpt-4-turbo-preview",
-      "messages": [
-        {
-          "role": "system",
-          "content":
-              "You are a helpful assistant. You need to summarize the text given by the user. But you can only answer all questions in English."
-        },
-        {"role": "user", "content": text},
-      ],
-      // "response_format": {"type": "json_object"}
-    }),
-  );
-  print("GPT replied\n\n\n");
-  print("Reply Data ${response.body}");
-  // return response.body;
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
-    final rawData = data['choices'][0]['message']['content'].toString();
-    final summary = utf8.decode(rawData.runes.toList());
-    print("Summary $summary\n\n\n");
-    return summary;
-    // return respons;
-  } else {
-    print("Failed\n");
-    return "Failed";
-  }
+    {"role": "user", "content": text},
+  ];
+  return callAPI(msgs);
 }
