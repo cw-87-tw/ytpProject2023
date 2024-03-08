5ee5b59ef44c4e2950399566e11f9fbfa2a0ff11
oTaiwan <66476837+cw-87-tw@users.noreply.github.com>
2024-03-09 00:05:54 +0800

    .

--- summarease/lib/process/textSummary.dart
+++ summarease/lib/process/textSummary.dart
@@ -47,14 +47,14 @@ Future<String> summarizeText(String text) async {
   
 }
 
-Future<String> sendAPIMessage(List chatMsgs, String summary) async {
+Future<String> sendAPIMessage(List chatMsgs, String script) async {
   List msgs = [
     {
       "role": "system",
       "content":
           "You are a helpful assistant. You need to summarize the text given by the user. If you want to answer in Chinese, please answer in Traditional Chinese"
     },
-    {"role": "user", "content": summary},
+    {"role": "user", "content": script},
   ];
   msgs.addAll(chatMsgs);
   return await callAPI(msgs);
