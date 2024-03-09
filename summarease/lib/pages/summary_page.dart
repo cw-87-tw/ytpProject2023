9994f3446aebc15994d3d8d2355401e0c1b6c62e
amyfang0404 <amyfang0404@gmail.com>
2024-03-09 23:06:40 +0800

    hi

--- summarease/lib/pages/summary_page.dart
+++ summarease/lib/pages/summary_page.dart
@@ -56,110 +56,115 @@ class _SummaryPageState extends State<SummaryPage> {
       appBar: AppBar(
         title: Text("對話")
       ),
-      body: Column(
-        children: [
-          //show conversation
-          StreamBuilder(
-            stream: conversation.getConversation(widget.videoIndex), 
-            builder: (context, snapshot) {
-
-              //if error
-              if (snapshot.hasError) {
-                return Center(child: Text("Something went wrong"));
-              }
-
-              //show loading circle
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              //get conversation
-              var jsonData = jsonDecode(snapshot.data!['conversation']);
-              msgs = jsonData['messages'];
-
-              //if no msgs (which shouldn't happen at all...?)
-              if (snapshot.data == null || msgs.isEmpty) {
-                return Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(25),
-                    child: Text("沒有對話紀錄"),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Column(
+          children: [
+            //show conversation
+            StreamBuilder(
+              stream: conversation.getConversation(widget.videoIndex), 
+              builder: (context, snapshot) {
+        
+                //if error
+                if (snapshot.hasError) {
+                  return Center(child: Text("Something went wrong"));
+                }
+        
+                //show loading circle
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+        
+                //get conversation
+                var jsonData = jsonDecode(snapshot.data!['conversation']);
+                msgs = jsonData['messages'];
+        
+                //if no msgs (which shouldn't happen at all...?)
+                if (snapshot.data == null || msgs.isEmpty) {
+                  return Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(25),
+                      child: Text("沒有對話紀錄"),
+                    ),
+                  );
+                }
+        
+                //show msgs as list
+                return Expanded(
+                  child: ListView.builder(
+                    controller: scroll_controller,
+                    shrinkWrap: true,
+                    itemCount: msgs.length,
+                    itemBuilder: (context, index) {
+                      return MsgTile(
+                        role: msgs[index]["role"].toString(), 
+                        content: msgs[index]["content"].toString(),
+                      );
+                    },
                   ),
                 );
               }
-
-              //show msgs as list
-              return Expanded(
-                child: ListView.builder(
-                  controller: scroll_controller,
-                  shrinkWrap: true,
-                  itemCount: msgs.length,
-                  itemBuilder: (context, index) {
-                    return MsgTile(
-                      role: msgs[index]["role"].toString(), 
-                      content: msgs[index]["content"].toString(),
-                    );
-                  },
-                ),
-              );
-            }
-          ),
-
-          //page down button
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: FloatingActionButton(
-              backgroundColor: Theme.of(context).colorScheme.surface,
-              elevation: 0,
-              child: Icon(Icons.arrow_downward, color: Colors.black,),
-              onPressed: scrollToBottom
             ),
-          ),
-
-          //textfield & send button
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0, bottom: 25.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: TextField(
-                      controller: msg_controller,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+            
+            SizedBox(height: 10,),
+        
+            //page down button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: FloatingActionButton(
+                backgroundColor: Theme.of(context).colorScheme.surface,
+                elevation: 0,
+                child: Icon(Icons.arrow_downward, color: Colors.black,),
+                onPressed: scrollToBottom
+              ),
+            ),
+        
+            //textfield & send button
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: TextField(
+                        controller: msg_controller,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          labelText: 'Ask ChatGPT something...',
+                          labelStyle: TextStyle(color: Colors.grey)
                         ),
-                        labelText: 'Ask ChatGPT something...',
-                        labelStyle: TextStyle(color: Colors.grey)
                       ),
                     ),
                   ),
-                ),
-                SendButton(
-                  onTap: () async {
-                    //if empty
-                    if (msg_controller.text.isEmpty) return;
-            
-                    //add user msg to list
-                    msgs.add({"role" : "user", "content" : msg_controller.text});
-                    msg_controller.clear();
-
-                    //call chatgpt
-                    String aiMsg = await sendAPIMessage(msgs, "");
-                    msgs.add({"role" : "system", "content" : aiMsg});
-
-                    //update conversation
-                    await conversation.updateConversation(msgs, widget.videoIndex);
-
-                    //auto scroll
-                    scrollToBottom();
-
-                  }
-                ),
-              ],
-            ),
-          )
-        ],
+                  SendButton(
+                    onTap: () async {
+                      //if empty
+                      if (msg_controller.text.isEmpty) return;
+              
+                      //add user msg to list
+                      msgs.add({"role" : "user", "content" : msg_controller.text});
+                      msg_controller.clear();
+        
+                      //call chatgpt
+                      String aiMsg = await sendAPIMessage(msgs, "");
+                      msgs.add({"role" : "system", "content" : aiMsg});
+        
+                      //update conversation
+                      await conversation.updateConversation(msgs, widget.videoIndex);
+        
+                      //auto scroll
+                      scrollToBottom();
+        
+                    }
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
