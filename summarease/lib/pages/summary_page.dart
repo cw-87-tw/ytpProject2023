259d49aa6eb8bbcb0dd742d48cf92451f505cdec
amyfang0404 <amyfang0404@gmail.com>
2024-03-11 22:27:18 +0800

    hi

--- summarease/lib/pages/summary_page.dart
+++ summarease/lib/pages/summary_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:summarease/process/sendEmail.dart';
@@ -178,8 +179,18 @@ class _SummaryPageState extends State<SummaryPage> {
                     //hide phone keyboard
                     FocusScope.of(context).requestFocus(FocusNode());
 
-                    //call chatgpt
-                    String aiMsg = await sendAPIMessage(msgs, "");
+                    String script = "";
+
+                    //call chatgpt  
+                    DocumentReference vid = 
+                      FirebaseFirestore.instance.collection('userFile')
+                      .doc(user!.uid).collection('userVideos').doc('video #${widget.videoIndex}');
+                    vid.get().then((doc) {
+                      final data = doc.data() as Map<String, dynamic>;
+                      script = data['script'];
+                    });
+
+                    String aiMsg = await sendAPIMessage(msgs, script);
                     msgs.add({"role": "system", "content": aiMsg});
 
                     //update chatgpt message
